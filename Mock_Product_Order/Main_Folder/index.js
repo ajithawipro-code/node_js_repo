8a801464282a672946bb3b87612b995d40dfcda8
Ajitha <ajitha.wipro@gmail.com>
2026-01-21 20:22:17 +0530

    commit after analytics added

--- Mock_Product_Order/Main_Folder/index.js
+++ Mock_Product_Order/Main_Folder/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { OrderRoute } from "./routes/orders.routes.js";
+import { AnalyticsRoute } from "./routes/analytics.routes.js";
 
 const app=express();
 app.use(express.json());
 
 app.use("/", OrderRoute);
+app.use("/analytics",AnalyticsRoute)
 
 app.listen(4000,()=>{
 
--- Mock_Product_Order/Main_Folder/routes/analytics.routes.js
+++ Mock_Product_Order/Main_Folder/routes/analytics.routes.js
@@ -1,5 +1,56 @@
 import express from "express";
+import { readData } from "../models/db.model.js";
 
 export const AnalyticsRoute=express.Router();
 
-AnalyticsRoute.get("/")
+AnalyticsRoute.get("/allorders",(req,res)=>{
+
+    const data=readData();
+    const orders=data.orders;
+
+    const totalcount=data.orders.length;
+
+    res.status(200).json({message:"Total Count",totalcount,orders});
+
+
+});
+
+AnalyticsRoute.get("/cancelled-orders",(req,res)=>{
+
+    const data=readData();
+
+    const cancelled_orders= data.orders.filter((el)=>el.status==="cancelled");
+
+    res.status(200).json({message:"Cancelled Orders", cancelled_orders});
+
+
+});
+
+
+AnalyticsRoute.get("/shipped",(req,res)=>{
+
+    const data=readData();
+
+    const shipped_orders= data.orders.filter((el)=>el.status==="shipped");
+
+    const count=shipped_orders.length;
+
+    res.status(200).json({message:"Shipped Orders Count", count,shipped_orders});
+
+
+});
+
+AnalyticsRoute.get("/total-revenue/:productId",(req,res)=>{
+
+    const data=readData();
+
+    const {productId}=req.params;
+
+    const orders=data.orders.filter((el)=>el.id===Number(productId));
+
+    const final_orders=orders.filter((el)=>el.status==="cancelled");
+
+    
+
+
+});
